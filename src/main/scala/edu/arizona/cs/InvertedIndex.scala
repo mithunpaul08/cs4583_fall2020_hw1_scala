b6dc81880f2e1a853bcde93b0055f82b66ebcd4b
mithun <mithunpaul08@gmail.com>
2020-08-30 18:29:44 -0700

    code reads file succesfully in laptop

--- src/main/scala/edu/arizona/cs/InvertedIndex.scala
+++ src/main/scala/edu/arizona/cs/InvertedIndex.scala
@@ -1,44 +1,52 @@
 package edu.arizona.cs
 
 import InvertedIndex._
-
+import scala.io.Source
 //remove if not needed
 import scala.collection.JavaConversions._
 
 object InvertedIndex {
 
   def main(args: Array[String]): Unit = {
-    try {
       val fileName: String = "Docs.txt"
       println("********Welcome to  Homework 1!")
       val query: String = "schizophrenia AND drug"
       val objInvertedIndex: InvertedIndex = new InvertedIndex(fileName)
-      val ans2: Array[String] = objInvertedIndex.runQ5_2(query)
-    } catch {
-      case ex: Exception => println(ex.getMessage)
-
-    }
+      val ans1: Array[String] = objInvertedIndex.runQ5_1(query)
   }
-
 }
 
-class InvertedIndex(var inputFilePath: String) {
 
-  var indexExists: Boolean = false
+
+class InvertedIndex(var inputFilePath: String) {
+  var indexExists = false
+  def buildIndex(): Unit = {
+    val source = Source.fromResource(inputFilePath)
+    for (line <- source.getLines()) {
+      println(line)
+//      ##do whatever you want to do with the data
+    }
+    source.close()
+    indexExists = true
+  }
 
   def runQ5_1(query: String): Array[String] = {
+    if(! indexExists) buildIndex()
     val dummy_ans: Array[String] = Array("Doc1", "Doc4")
-    dummy_ans
+    return dummy_ans
   }
 
   def runQ5_2(query: String): Array[String] = {
     val dummy_ans: Array[String] = Array("Doc1", "Doc2", "Doc3", "Doc4")
-    dummy_ans
+    return dummy_ans
   }
 
   def runQ5_3(query: String): Array[String] = {
     val dummy_ans: Array[String] = Array("Doc1", "Doc2", "Doc4")
-    dummy_ans
+    return dummy_ans
   }
+
+
+
 
 }

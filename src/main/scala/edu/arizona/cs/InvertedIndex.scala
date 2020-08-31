66c1c56af4eb6e5b31c73165ca1b96d3e539c2d8
mithun <mithunpaul08@gmail.com>
2020-08-31 16:58:19 -0700

    added while loop so as to take continuous user input

--- src/main/scala/edu/arizona/cs/InvertedIndex.scala
+++ src/main/scala/edu/arizona/cs/InvertedIndex.scala
@@ -8,14 +8,17 @@ import scala.collection.JavaConversions._
 object InvertedIndex {
 
   def main(args: Array[String]): Unit = {
-      val fileName: String = "Docs.txt"
-      println("********Welcome to  Homework 1 Qn 5!")
-      val name = readLine("Which one of the 3 sub questions would you like to test. Press 1 for qn1, 2 for qn2 and 3 for qn 3 ")
+    val fileName: String = "Docs.txt"
+    println("********Welcome to  Homework 1 Qn 5!")
     val objInvertedIndex: InvertedIndex = new InvertedIndex(fileName)
-    if name=="1" {
-      val query: String = "schizophrenia AND drug"
-
-      val ans1: Array[String] = objInvertedIndex.runQ5_1 (query)
+    while (true) {
+      val qn = scala.io.StdIn.readLine("Which one of the 3 sub questions would you like to test. " +
+        "Press 1 for qn1, 2 for qn2 and 3 for qn 3. 0 to exit: ")
+      val qn_int = qn.toInt
+      if (qn_int == 1) {
+        val query = scala.io.StdIn.readLine("Enter query to process: ")
+        //val ans1 = objInvertedIndex.runQ5_1(query)
+      }
     }
   }
 }
@@ -26,16 +29,15 @@ class InvertedIndex(var inputFilePath: String) {
   var indexExists = false
   def buildIndex(): Unit = {
     if(! indexExists) buildIndex()
-    {
-    val source = Source.fromResource(inputFilePath)
-    for (line <- source.getLines()) {
-      println(line)
-      // create index with the data
-    }
-    
+      val source = Source.fromResource(inputFilePath)
+      for (line <- source.getLines()) {
+        println(line)
+        // create index with the data
+      }
+
     source.close()
     indexExists = true
-    }
+
   }
 
   def runQ5_1(query: String): Array[String] = {
